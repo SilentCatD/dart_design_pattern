8d38057eae9ec7501510acb67d477ea4bce312c9
SilentCat <ngocphuocnguyen312@gmail.com>
2022-09-27 15:05:59 +0700

    prototype

--- lib/design_pattern/builder.dart
+++ lib/design_pattern/builder.dart
@@ -1,5 +1,5 @@
-/// Builder is a creational design pattern that lets you construct complex o
-/// bjects step by step. The pattern allows you to produce different types and
+/// Builder is a creational design pattern that lets you construct complex
+/// objects step by step. The pattern allows you to produce different types and
 /// representations of an object using the same construction code.
 
 abstract class Wall {}
